dca13e7af33ab10002bfe15c39580c2a450479f8
artF412 <Chalantorn.sup@gmail.com>
2022-09-21 15:27:55 +0700

    gpa&info

---
+++ gpaScoreAvg.py
@@ -0,0 +1,13 @@
+thaiscore = float(input(""))
+mathscore = float(input(""))
+engscore = float(input(""))
+sciscore = float(input(""))
+sportscore = float(input(""))
+print("THAI =",thaiscore)
+print("MATH =",mathscore)
+print("ENGLISH =",engscore)
+print("SCIENCE =",sciscore)
+print("SPORT =",sportscore)
+print("---")
+gpa = (thaiscore+mathscore+engscore+sciscore+sportscore)/5
+print("GPA =",gpa)
