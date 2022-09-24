1d69f3a06ad6c0662f671aca2f578afd8b984f65
artF412 <Chalantorn.sup@gmail.com>
2022-09-25 00:00:20 +0700

    Create minAndMax.py

---
+++ minAndMax.py
@@ -0,0 +1,6 @@
+num1 = int(input("Number 1 : "))
+num2 = int(input("Number 2 : "))
+num3 = int(input("Number 3 : "))
+num4 = int(input("Number 4 : "))
+num5 = int(input("Number 5 : "))
+print(str(min(num1,num2,num3,num4,num5))+","+str(max(num1,num2,num3,num4,num5)))
