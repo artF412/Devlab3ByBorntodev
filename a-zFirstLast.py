3daa2231690083a129f520d3f876b9718d3a9d3b
artF412 <Chalantorn.sup@gmail.com>
2022-10-01 23:02:54 +0700

    Create a-zFirstLast.py

---
+++ a-zFirstLast.py
@@ -0,0 +1,14 @@
+name1 = input("Name 1 : ")
+name2 = input("Name 2 : ")
+choose = input("Select first or last : ")
+
+if choose == "first":
+    if name1 < name2: # check if name1 is in front of name2 (alphabetical)
+        print(name1)
+    else:
+        print(name2)
+else:
+    if name1 > name2: # check if name1 is after name2 (alphabetical)
+        print(name1)
+    else:
+        print(name2)
