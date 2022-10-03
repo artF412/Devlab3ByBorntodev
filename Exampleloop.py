63d6b9e2c839c9bc0517bef0bc554812b447109d
artF412 <Chalantorn.sup@gmail.com>
2022-10-04 00:24:30 +0700

    Create Exampleloop.py

---
+++ Exampleloop.py
@@ -0,0 +1,9 @@
+start , stop =1,5
+sum,avg = 0,0
+while(start<=stop):
+    number = int(input("ป้อนตัวเลขของคุณ : "))
+    sum+=number
+    start+=1
+avg=sum/stop
+print("ผลรวม =",sum)
+print("ค่าเฉลี่ย =",avg)
