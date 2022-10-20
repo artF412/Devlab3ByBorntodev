f037980b8bfa6a80462ecd8df1f2a071ebbb9158
artF412 <Chalantorn.sup@gmail.com>
2022-10-20 15:58:05 +0700

    Update feb2829Devlab.py

--- feb2829Devlab.py
+++ feb2829Devlab.py
@@ -1,6 +1,8 @@
 years = int(input("รับค่าปี ค.ศ. : "))
 sum = years%4
-if sum == 0:
+if years == 1800:
+    print("Not a Leap Year")
+elif sum == 0:
     print("Leap Year")
 else:
     print("Not a Leap Year")
